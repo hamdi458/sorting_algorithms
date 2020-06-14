35d358342bfac0a9a64c4dbffee891d88e2a5195
hamdi458 <hamdi.ghorbel78@gmail.com>
2020-06-14 17:19:37 +0000

    selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,9 +11,10 @@ void selection_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
+		jmin = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[i])
+			if (array[j] < array[jmin])
 				jmin = j;
 		}
 		if (jmin != i)
